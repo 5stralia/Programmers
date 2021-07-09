747cffb52ff666733fc076ca88455591d5bbb9c4
hoju <5stralia@gmail.com>
2021-07-09 23:17:12 +0900

    P12985
    https://programmers.co.kr/learn/courses/30/lessons/12985#

---
+++ Programmers/Programmers/Problems/P12985.swift
@@ -0,0 +1,36 @@
+//
+//  P12985.swift
+//  Programmers
+//
+//  Created by 최호주 on 2021/07/10.
+//
+
+import Foundation
+
+class P12985 {
+    func solution(_ n:Int, _ a:Int, _ b:Int) -> Int {
+        var answer = 0
+        
+        var player = n
+        var A = a
+        var B = b
+        while(player > 0) {
+            answer += 1
+            player = player / 2
+            
+            A = nextNumber(A)
+            B = nextNumber(B)
+            
+            if A == B {
+                return answer
+            }
+        }
+        
+        return answer
+    }
+    
+    func nextNumber(_ n: Int) -> Int {
+        return (n + 1) / 2
+    }
+}
+f
