8817e54135c6ee593c6fa0d041bcc2eede746fb0
hoju <5stralia@gmail.com>
2021-08-03 22:56:44 +0900

    P12913 수정

--- Programmers/Programmers/Problems/P12913.swift
+++ Programmers/Programmers/Problems/P12913.swift
@@ -12,9 +12,6 @@ class P12913 {
         var answer = land
         
         for i in (1..<answer.count) {
-            
-        }
-        for i in (1..<answer.count) {
             answer[i][0] += max(answer[i-1][1], answer[i-1][2], answer[i-1][3])
             answer[i][1] += max(answer[i-1][0], answer[i-1][2], answer[i-1][3])
             answer[i][2] += max(answer[i-1][0], answer[i-1][1], answer[i-1][3])
